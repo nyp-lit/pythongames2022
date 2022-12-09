d2803acd135dafda6e5f918d08dcfaed6665191e
siinyann <sinyanchong4@gmail.com>
2022-12-09 23:05:20 +0800

    Change to 2 player codes

--- Tic-Tac-Toe/app.py
+++ Tic-Tac-Toe/app.py
@@ -1,43 +1,176 @@
-# Step 1: pip install flask and import the following modules
-from flask import Flask, render_template, url_for, jsonify, request
-# Step 6: Import logic.py into app.py
-from logic import Easy, Medium, Hard
+# # Step 1: pip install flask and import the following modules
+# from flask import Flask, render_template, url_for, jsonify, request
+# # Step 6: Import logic.py into app.py
+# from logic import Easy, Medium, Hard
 
-# Step 2: Create app with flask(__name__)
+# # Step 2: Create app with flask(__name__)
+# app = Flask(__name__)
+
+# # Step 3: Add a new folder called templates and 
+# # add a new html file called index.html into the folder
+# # Step 4: Create route for index.html
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/move',methods=['POST'])
+# def move():
+#     state = request.get_json()
+    
+#     difficulty = state.get('difficulty')
+    
+#     # Choose game mode based on difficulty
+#     if (difficulty == 0):
+#         game = Easy()
+#     elif (difficulty == 1):
+#         game = Medium()
+#     elif (difficulty == 2):
+#         game = Hard()
+        
+#     game.board = state.get('board')
+#     game.player = state.get('player')
+#     game.computer = state.get('computer')
+    
+#     # Calculate computer move
+#     move = game.calculate_move()
+    
+#     # Response sent with calculated move
+#     return jsonify(computerMove = move)
+    
+    
+# # Step 5: Add app.run with debug = true to run app.py
+# if __name__ == "__main__":
+#     app.run(debug=True)
+# # Step 1: pip install flask and import the following modules
+# from flask import Flask, render_template, url_for, jsonify, request
+# # Step 6: Import logic.py into app.py
+# from logic import Easy, Medium, Hard
+
+# # Step 2: Create app with flask(__name__)
+# app = Flask(__name__)
+
+# # Step 3: Add a new folder called templates and 
+# # add a new html file called index.html into the folder
+# # Step 4: Create route for index.html
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/move',methods=['POST'])
+# def move():
+#     state = request.get_json()
+    
+#     difficulty = state.get('difficulty')
+    
+#     # Choose game mode based on difficulty
+#     if (difficulty == 0):
+#         game = Easy()
+#     elif (difficulty == 1):
+#         game = Medium()
+#     elif (difficulty == 2):
+#         game = Hard()
+        
+#     game.board = state.get('board')
+#     game.player = state.get('player')
+#     game.computer = state.get('computer')
+    
+#     # Calculate computer move
+#     move = game.calculate_move()
+    
+#     # Response sent with calculated move
+#     return jsonify(computerMove = move)
+    
+    
+# # Step 5: Add app.run with debug = true to run app.py
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+import os
+from flask import send_file
+import re
+import urllib.request
+from flask import Flask, flash, request, redirect, render_template,url_for,send_from_directory
+from werkzeug.utils import secure_filename
+import pathlib,os
+from flask import Flask, render_template, session, redirect, url_for
+from flask_session import Session
+#from tempfile import mkdtemp
+
+
 app = Flask(__name__)
 
-# Step 3: Add a new folder called templates and 
-# add a new html file called index.html into the folder
-# Step 4: Create route for index.html
-@app.route('/')
-def index():
-    return render_template('index.html')
+# app.config["SESSION_FILE_DIR"] = "\tmp" #this is a temp folder at heroku for temp data storing
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-@app.route('/move',methods=['POST'])
-def move():
-    state = request.get_json()
-    
-    difficulty = state.get('difficulty')
-    
-    # Choose game mode based on difficulty
-    if (difficulty == 0):
-        game = Easy()
-    elif (difficulty == 1):
-        game = Medium()
-    elif (difficulty == 2):
-        game = Hard()
-        
-    game.board = state.get('board')
-    game.player = state.get('player')
-    game.computer = state.get('computer')
-    
-    # Calculate computer move
-    move = game.calculate_move()
-    
-    # Response sent with calculated move
-    return jsonify(computerMove = move)
-    
-    
-# Step 5: Add app.run with debug = true to run app.py
+@app.route("/")
+def index():
+    if "board" not in session:
+        session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+        session["turn"] = "X"
+        session["winner"] = False
+        session["draw"] = False
+
+    winner = winnerFound(session["board"])
+
+    if(winner[0] == True):
+    	session["winner"] = True
+    	session["turn"] = winner[1]
+    	print("winner found",session["winner"], session["turn"])
+
+    elif(winner[0] == False and winner[1] == "draw"):
+    	session["draw"] = True
+
+    return render_template("index.html", game=session["board"], turn=session["turn"],winnerFound=session["winner"],winner=session["turn"],draw=session["draw"])
+
+@app.route("/play/<int:row>/<int:col>")
+def play(row, col):
+	session["board"][row][col] = session["turn"]
+	if(session["turn"] == "X"):
+		session["turn"] = "O"
+	else:
+		session["turn"] = "X"
+
+	return redirect(url_for("index"))
+@app.route("/reset")
+def reset():
+	session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+	session["turn"] = "X"
+	session["winner"] = False
+	session["draw"] = False
+	return redirect(url_for("index"))
+	
+def winnerFound(board):
+	#print("here at winner")
+	#check rows
+	for i in range(len(board)):
+		if(board[i][0] == None):
+			break
+		if (board[i][0] == board[i][1] and  board[i][0] == board[i][2]):
+			return [True, board[i][0]]
+	#check cols
+	for i in range(len(board)):
+		if(board[0][i] == None):
+			break
+		if (board[0][i] == board[1][i] and board[0][i] == board[2][i]):
+			return [True, board[0][i]]
+	#check diagonals
+	if(board[0][0] == board[1][1] and board[0][0] == board[2][2]):
+		if(board[0][0] != None):
+			return [True, board[0][0]]
+	#check diagonals
+	if(board[2][0] == board[1][1] and board[2][0] == board[0][2]):
+		if(board[2][0] != None):
+			return [True, board[2][0]]
+	#check if game is drawn
+	for i in range(len(board)):
+		for j in range(len(board)):
+			if(board[i][j] == None):
+				return [False, board[0][0]]
+
+	#game is drawn since there is no winner 
+	#and all boxes are filled
+	return [False, "draw"]
 if __name__ == "__main__":
     app.run(debug=True)
--- Tic-Tac-Toe/logic.py
+++ Tic-Tac-Toe/logic.py
@@ -1,160 +1,160 @@
-# Step 1: import the following modules
-import random
-from copy import deepcopy
+# # Step 1: import the following modules
+# import random
+# from copy import deepcopy
 
-# Step 2: Create a Game class
-class Game:
-    # Inialize the player, computer and board
-    def __init__(self, player_one = 'o', player_two = 'x'):
-        self.player_one = player_one
-        self.player_two = player_two
-        self.board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+# # Step 2: Create a Game class
+# class Game:
+#     # Inialize the player, computer and board
+#     def __init__(self, player_one = 'o', player_two = 'x'):
+#         self.player_one = player_one
+#         self.player_two = player_two
+#         self.board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
         
-    # Returns a copy of the board to experiment on
-    def board_copy(self):
-        return deepcopy(self.board)
+#     # Returns a copy of the board to experiment on
+#     def board_copy(self):
+#         return deepcopy(self.board)
     
-    # Returns True if a particular position in the board is empty
-    def position_empty(self,pos):
-        if (self.board[pos] == ' '):
-            return True
-        else:
-            return False
+#     # Returns True if a particular position in the board is empty
+#     def position_empty(self,pos):
+#         if (self.board[pos] == ' '):
+#             return True
+#         else:
+#             return False
     
-    # Returns a list of possible moves
-    def get_possible_moves(self):
-        moves= []
-        for i in range(len(self.board)):
-            if (self.position_empty(i)):
-                moves.append(i)
-        return moves
+#     # Returns a list of possible moves
+#     def get_possible_moves(self):
+#         moves= []
+#         for i in range(len(self.board)):
+#             if (self.position_empty(i)):
+#                 moves.append(i)
+#         return moves
     
-    # Returns true if the board is full
-    def is_board_full(self):
-        for i in range(0, len(self.board)):
-            if (self.board[i] == ' '):
-                return False
-        return True
+#     # Returns true if the board is full
+#     def is_board_full(self):
+#         for i in range(0, len(self.board)):
+#             if (self.board[i] == ' '):
+#                 return False
+#         return True
     
-    # Returns the winning combination or returns False
-    def has_won(self,board, mark):
-        # Horizontal Win
-        if board[0] == board[1] and board[0] == board[2] and board[0] == mark:
-            return [[0,1,2]]
-        elif board[3] == board[4] and board[3] == board[5] and board[3] == mark:
-            return [[3,4,5]]
-        if board[6] == board[7] and board[6] == board[8] and board[6] == mark:
-            return [[6,7,8]]
+#     # Returns the winning combination or returns False
+#     def has_won(self,board, mark):
+#         # Horizontal Win
+#         if board[0] == board[1] and board[0] == board[2] and board[0] == mark:
+#             return [[0,1,2]]
+#         elif board[3] == board[4] and board[3] == board[5] and board[3] == mark:
+#             return [[3,4,5]]
+#         if board[6] == board[7] and board[6] == board[8] and board[6] == mark:
+#             return [[6,7,8]]
         
-        # Vertical Win
-        elif board[0] == board[3] and board[0] == board[6] and board[0] == mark:
-            return [[0,3,6]]
-        elif board[1] == board[4] and board[1] == board[7] and board[1] == mark:
-            return [[1,4,7]]
-        if board[2] == board[5] and board[2] == board[8] and board[2] == mark:
-            return [[2,5,8]]
+#         # Vertical Win
+#         elif board[0] == board[3] and board[0] == board[6] and board[0] == mark:
+#             return [[0,3,6]]
+#         elif board[1] == board[4] and board[1] == board[7] and board[1] == mark:
+#             return [[1,4,7]]
+#         if board[2] == board[5] and board[2] == board[8] and board[2] == mark:
+#             return [[2,5,8]]
         
-        # Diagonal Win
-        elif board[0] == board[4] and board[0] == board[8] and board[0] == mark:
-            return [[0,4,8]]
-        elif board[2] == board[4] and board[2] == board[6] and board[2] == mark:
-            return [[2,4,6]]
-        else:
-            return False
+#         # Diagonal Win
+#         elif board[0] == board[4] and board[0] == board[8] and board[0] == mark:
+#             return [[0,4,8]]
+#         elif board[2] == board[4] and board[2] == board[6] and board[2] == mark:
+#             return [[2,4,6]]
+#         else:
+#             return False
         
-# Step 3: Create Easy class for Easy mode
-# Easy bot
-#   Picks winning moves. 
-#   Else, it makes random moves.
-class Easy(Game):
+# # Step 3: Create Easy class for Easy mode
+# # Easy bot
+# #   Picks winning moves. 
+# #   Else, it makes random moves.
+# class Easy(Game):
     
-    def calulate_move(self):
+#     def calulate_move(self):
         
-        moves = self.get_possible_moves()
-        if (len(moves) == 0):
-            return -1
+#         moves = self.get_possible_moves()
+#         if (len(moves) == 0):
+#             return -1
         
-        # Check for winning move
-        for move in moves:
-            board_copy = self.board_copy()
-            board_copy[move] = self.player_two
-            if (self.has_won(board_copy,self.player_two)):
-                return move
+#         # Check for winning move
+#         for move in moves:
+#             board_copy = self.board_copy()
+#             board_copy[move] = self.player_two
+#             if (self.has_won(board_copy,self.player_two)):
+#                 return move
             
-        # Return a random move
-        # return random.choice(moves)
+#         # Return a random move
+#         # return random.choice(moves)
 
-# Step 4: Create Medium class for Medium mode
-# Medium bot
-# Picks winning moves
-# Blocks opponent's win opportunities
-# Else makes random moves
-class Medium(Game):
+# # Step 4: Create Medium class for Medium mode
+# # Medium bot
+# # Picks winning moves
+# # Blocks opponent's win opportunities
+# # Else makes random moves
+# class Medium(Game):
     
-    def calculate_move(self):
+#     def calculate_move(self):
         
-        moves = self.get_possible_moves()
-        if (len(moves) == 0):
-            return -1
+#         moves = self.get_possible_moves()
+#         if (len(moves) == 0):
+#             return -1
         
-        # Check for winning move
-        for move in moves:
-            board_copy = self.board_copy()
-            board_copy[move] = self.player_two
-            if (self.has_won(board_copy,self.player_two)):
-                return move
+#         # Check for winning move
+#         for move in moves:
+#             board_copy = self.board_copy()
+#             board_copy[move] = self.player_two
+#             if (self.has_won(board_copy,self.player_two)):
+#                 return move
             
-        # Check if opponent can win next turn and block that move
-        for move in moves:
-            board_copy = self.board_copy()
-            board_copy[move] = self.player_one
-            if(self.has_won(board_copy,self.player_one)):
-                return move
+#         # Check if opponent can win next turn and block that move
+#         for move in moves:
+#             board_copy = self.board_copy()
+#             board_copy[move] = self.player_one
+#             if(self.has_won(board_copy,self.player_one)):
+#                 return move
 
-        # Return a random move
-        # return random.choice(moves)
+#         # Return a random move
+#         # return random.choice(moves)
 
-# Step 6: Create Hard class for Hard mode
-# Hard bot
-#   Picks winning moves. 
-#   Blocks opponent's win opportunities. 
-#   Else, makes selected moves based on game theory.
-class Hard(Game):
+# # Step 6: Create Hard class for Hard mode
+# # Hard bot
+# #   Picks winning moves. 
+# #   Blocks opponent's win opportunities. 
+# #   Else, makes selected moves based on game theory.
+# class Hard(Game):
     
-    def calculate_move(self):
+#     def calculate_move(self):
         
-        moves = self.get_possible_moves()
-        if (len(moves) == 0):
-            return -1
+#         moves = self.get_possible_moves()
+#         if (len(moves) == 0):
+#             return -1
         
-        # Check for winning move
-        for move in moves:
-            board_copy = self.board_copy()
-            board_copy[move] = self.player_two
-            if (self.has_won(board_copy,self.player_two)):
-                return move
+#         # Check for winning move
+#         for move in moves:
+#             board_copy = self.board_copy()
+#             board_copy[move] = self.player_two
+#             if (self.has_won(board_copy,self.player_two)):
+#                 return move
             
-        # Check if opponent can win next turn and block that move
-        for move in moves:
-            board_copy = self.board_copy()
-            board_copy[move] = self.player_one
-            if(self.has_won(board_copy,self.player_one)):
-                return move
+#         # Check if opponent can win next turn and block that move
+#         for move in moves:
+#             board_copy = self.board_copy()
+#             board_copy[move] = self.player_one
+#             if(self.has_won(board_copy,self.player_one)):
+#                 return move
             
-        # corners = [0,2,6,8]
-        # middle = [4]
-        # other = [1,3,5,7]
+#         # corners = [0,2,6,8]
+#         # middle = [4]
+#         # other = [1,3,5,7]
         
-        # # Choose corners if possible
-        # moveset = list(set(corners).intersection(set(moves)))
-        # if (len(moveset) != 0):
-        #     return random.choice(moveset)
+#         # # Choose corners if possible
+#         # moveset = list(set(corners).intersection(set(moves)))
+#         # if (len(moveset) != 0):
+#         #     return random.choice(moveset)
         
-        # # Choose middle cell if corners are taken
-        # moveset = list(set(middle).intersection(set(moves)))
-        # if (len(moveset) != 0):
-        #     return random.choice(moveset)
+#         # # Choose middle cell if corners are taken
+#         # moveset = list(set(middle).intersection(set(moves)))
+#         # if (len(moveset) != 0):
+#         #     return random.choice(moveset)
         
-        # # Make a choice from one of the remaining cells.
-        # moveset = list(set(other).intersection(set(moves)))
-        # return random.choice(moveset)
+#         # # Make a choice from one of the remaining cells.
+#         # moveset = list(set(other).intersection(set(moves)))
+#         # return random.choice(moveset)
