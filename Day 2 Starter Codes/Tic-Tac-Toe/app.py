b33dfbc1aeef2d74c52f4c94ad788e1c0c244624
siinyann <sinyanchong4@gmail.com>
2022-12-14 20:00:49 +0800

    Added starter codes for tic-tac-toe

---
+++ Day 2 Starter Codes/Tic-Tac-Toe/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, session, redirect, url_for
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/play/<int:row>/<int:col>")
+def play():
+    return redirect(url_for("index"))
+
+@app.route("/reset")
+def reset():
+    return redirect(url_for("index"))
+    
+# def winnerFound():
+    
+if __name__ == "__main__":
+    app.run(debug=True)
